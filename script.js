9c6928ec0326b12adb3c937006f419bb401b9256
Francisco Neto <wantedshark@gmail.com>
2023-01-27 14:47:12 -0300

    Scroll orientation added in repositories and projects

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-const titleContainer = document.querySelector(".title-container")
+const repositorios = document.querySelector(".repositorios-container")
+const reposSetaR = document.querySelector(".repos-seta--right")
+const reposSetaL = document.querySelector(".repos-seta--left")
+reposSetaL.style.animation = "none"
+reposSetaL.style.color = "darkgray"
 
-function blinkBorder() {
-    setInterval(() => {
-        titleContainer.style.borderRight = "none"
-    }, 500);
-    setInterval(() => {
-        titleContainer.style.borderRight = "1px solid #000"
-    }, 1000);
-}
-blinkBorder()
+const projetos = document.querySelector(".projetos-container")
+const projSetaR = document.querySelector(".proj-seta--right")
+const projSetaL = document.querySelector(".proj-seta--left")
+projSetaL.style.animation = "none"
+projSetaL.style.color = "darkgray"
+
+repositorios.onscroll = function () {
+    if (this.scrollLeft === 0) {
+        reposSetaR.style.animation = "moveToLeft 2s ease-in-out infinite"
+        reposSetaR.style.color = "#000"
+        reposSetaL.style.animation = "none"
+        reposSetaL.style.color = "darkgray"
+    }
+    if (this.scrollLeft == 1829) {
+        reposSetaL.style.animation = "moveToLeft 2s ease-in-out infinite"
+        reposSetaL.style.color = "#000"
+        reposSetaR.style.animation = "none"
+        reposSetaR.style.color = "darkgray"
+    }
+};
+
+projetos.onscroll = function () {
+    if (this.scrollLeft === 0) {
+        projSetaR.style.animation = "moveToLeft 2s ease-in-out infinite"
+        projSetaR.style.color = "#000"
+        projSetaL.style.animation = "none"
+        projSetaL.style.color = "darkgray"
+    }
+    if (this.scrollLeft == 1829) {
+        projSetaL.style.animation = "moveToLeft 2s ease-in-out infinite"
+        projSetaL.style.color = "#000"
+        projSetaR.style.animation = "none"
+        projSetaR.style.color = "darkgray"
+    }
+};
